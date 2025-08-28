cab43140f23f85e8b77a49ae12e64f0c6a1e48e5
Tailung1 <ramazsamitashvili11@gmail.com>
2025-08-28 23:03:42 +0200

    set up in built error for find All /Get request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+// middlewares can be written in any module, not only in app module
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'products', method: RequestMethod.GET });
+  }
+}
